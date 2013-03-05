52f7b1e88756bf6716db73620a1e317b0d094c03
Steffen Eichhorn <mail@indubio.org>
2013-03-05 22:01:22 +0100

    redirect to source or build gui version in dependence of DEBUG setting

--- PaDaES/qxgui/views.py
+++ PaDaES/qxgui/views.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
 from django.shortcuts import HttpResponseRedirect
 
 def index(request):
-    return HttpResponseRedirect('/static/padaes/source/index.html')
+    redirect_url = '/static/padaes/%s/index.html'
+    if settings.DEBUG:
+        redirect_url = redirect_url % ('source')
+    else:
+        redirect_url = redirect_url % ('build')
+    return HttpResponseRedirect(redirect_url)
